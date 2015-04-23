da9dc75e7fe76bb2a6741b14ff80a6ae05fd4767
Rohit Agrawal <rohit489@gmail.com>
2015-04-22 19:55:48 -0700

    Show popup with list of myfiles when invoice button is clicked
    Show popup with list of myfiles when invoice button is clicked

---
+++ src/MyShuttle.Client.UniversalApp/MyShuttle.Client.UniversalApp.Windows/Services/MyFilesOperation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Office365.SharePoint.CoreServices;
+using Microsoft.Office365.SharePoint.FileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShuttle.Client.UniversalApp.Services
+{
+    static class MyFilesOperation
+    {
+        public static async Task<List<IItem>> getMyFiles(SharePointClient spClient)
+        {
+            List<IItem> myFilesList = new List<IItem>();
+            var myFilesResult = await spClient.Files.ExecuteAsync();
+            do
+            {
+                var files = myFilesResult.CurrentPage;
+                foreach (var file in files)
+                {
+                    myFilesList.Add(file);
+                }
+                myFilesResult = await myFilesResult.GetNextPageAsync();
+            } while (myFilesResult != null);
+            return myFilesList;
+        }
+
+    }
+}
