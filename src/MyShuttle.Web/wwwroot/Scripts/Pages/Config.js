8fb72d8d5dfefae8ca15e3f054f523b9e8467ced
BethMassi <bethma@microsoft.com>
2015-04-28 14:17:54 -0700

    Update company address to Chicago

--- src/MyShuttle.Web/wwwroot/Scripts/Pages/Config.js
+++ src/MyShuttle.Web/wwwroot/Scripts/Pages/Config.js
@@ -2,10 +2,10 @@ var MyShuttle = MyShuttle || {};
 
 MyShuttle.Config = function(){
     var bingMapsKey = 'AowwaZssHfABfk67j7II30OYz2E4PF2qYsX3kSDLjokOyDLFR3HBozSlZY9gNb6e',
-        infobBoxCompanyAddress = 'Spring Studios, 50 Varick St',
+        infobBoxCompanyAddress = 'Spring Studios, 164 E Chicago Ave',
         companyLocation = {
-            Latitude: 40.72109533886229,
-            Longitude: -74.006596745813
+            Latitude: 41.896755,
+            Longitude: -87.623470
         },
         smallScreenMinWidth = 768;
 
